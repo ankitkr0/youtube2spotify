806f86e039a5a884889ee6d1e2ea73b86afeda99
Ankit Kumar <162094577+ankitkr0@users.noreply.github.com>
2024-04-22 13:49:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Securely store your credentials and other configuration settings
-YOUTUBE_API_KEY = 'AIzaSyClObmquHJixkKNgjowq8FLIGPl3ppyZlk'
-SPOTIFY_CLIENT_ID = '7eeebebb69034776a902a744da1e5496'
-SPOTIFY_CLIENT_SECRET = '5f4e75a1752d4813b8bb0b21d4c9aa3b'
+YOUTUBE_API_KEY = ''
+SPOTIFY_CLIENT_ID = ''
+SPOTIFY_CLIENT_SECRET = ''
